e3ee04e341287eb9bf9e850ae40e6b86003e45f3
Timo Wang <ntwang1994@gmail.com>
2020-02-15 02:21:48 -0600

    Attempt to create a model.

--- run_test.py
+++ run_test.py
@@ -7,16 +7,47 @@ from tirg.img_text_composition_models import TIRG
 from tirg.test_retrieval import test
 
 
+def load_fashion_dataset(dataset_path):
+    trainset = Fashion200k(
+        path=dataset_path,
+        split='train',
+        transform=torchvision.transforms.Compose([
+            torchvision.transforms.Resize(224),
+            torchvision.transforms.CenterCrop(224),
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize([0.485, 0.456, 0.406],
+                                             [0.229, 0.224, 0.225])
+        ]))
+    testset = Fashion200k(
+        path=dataset_path,
+        split='test',
+        transform=torchvision.transforms.Compose([
+            torchvision.transforms.Resize(224),
+            torchvision.transforms.CenterCrop(224),
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize([0.485, 0.456, 0.406],
+                                             [0.229, 0.224, 0.225])
+        ]))
+
+    print 'trainset size:', len(trainset)
+    print 'testset size:', len(testset)
+    return trainset, testset
+
 def main():
     argparser = argparse.ArgumentParser()
     argparser.add_argument('dataset_path', type=str)
-    argparser.add_argument('pretrained_model_path', type=str)
-
+    argparser.add_argument('checkpoint_path', type=str)
+    args = argparser.parse_args()
     dataset_path = args.dataset_path
-    pretrained_model_path = args.pretrained_model_path
+    checkpoint_path = args.checkpoint_path
+
+    trainset, testset = load_fashion_dataset(dataset_path)
+    checkpoint = torch.load(checkpoint_path)
+    model_state_dict = checkpoint['model_state_dict']
     
-    model = TIRG()
-    model.load_state_dict(torch.load(pretrained_model_path))
+    model = TIRG([t.decode('utf-8') for t in trainset.get_all_texts()], 512)
+    
+    print 'Successfully created the model'
     
 
 
