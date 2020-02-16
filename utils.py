94abeb3b7397f65961d5b7077626ef65f9d02045
Timo Wang <ntwang1994@gmail.com>
2020-02-16 05:42:17 -0600

    Finishing the first draft of an interactive query for images.

---
+++ utils.py
@@ -0,0 +1,28 @@
+from tirg.datasets import Fashion200k
+
+
+def load_fashion_dataset(dataset_path):
+    trainset = Fashion200k(
+        path=dataset_path,
+        split='train',
+        transform=torchvision.transforms.Compose([
+            torchvision.transforms.Resize(224),
+            torchvision.transforms.CenterCrop(224),
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize([0.485, 0.456, 0.406],
+                                             [0.229, 0.224, 0.225])
+        ]))
+    testset = Fashion200k(
+        path=dataset_path,
+        split='test',
+        transform=torchvision.transforms.Compose([
+            torchvision.transforms.Resize(224),
+            torchvision.transforms.CenterCrop(224),
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize([0.485, 0.456, 0.406],
+                                             [0.229, 0.224, 0.225])
+        ]))
+
+    print 'trainset size:', len(trainset)
+    print 'testset size:', len(testset)
+    return trainset, testset
